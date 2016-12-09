92cbbf860f5efce7d9a65ff4f1e14eb13e47a77d
Randy Glenn <randy@surrealitylabs.com>
2016-12-09 11:22:14 -0500

    Moved to common light control methods for manual and Homie control. Added Homie UI bundle. Enabled long press to reset and enter setup mode. Added special firmware name and version values to allow homie-ota to identify firmware properly.

--- esp8266Switch.ino
+++ esp8266Switch.ino
@@ -7,20 +7,46 @@ const int PIN_LED = 2;
 const int PIN_BUTTON = 13;
 
 
+#define FW_NAME "ecoplug"
+#define FW_VERSION "1.0.2"
+
+/* Magic sequence for Autodetectable Binary Upload */
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+/* End of magic sequence for Autodetectable Binary Upload */
+
 HomieNode switchNode("plug", "switch");
 Button button1(PIN_BUTTON); // Connect your button between pin 2 and GND
 
+static uint8_t lightState = LOW;
+
+void setLightState(uint8_t newLightState) {
+  if(newLightState == HIGH) {
+    digitalWrite(PIN_RELAY, HIGH);
+    switchNode.setProperty("on").send("true");
+    Serial.println("Light is on");
+    lightState = HIGH;
+  } else if(newLightState == LOW) {
+    digitalWrite(PIN_RELAY, LOW);
+    switchNode.setProperty("on").send("false");
+    Serial.println("Light is off");    
+    lightState = LOW;
+  }
+}
+
+void invertLightState() {
+  if(lightState == LOW) {
+    setLightState(HIGH);
+  } else if(lightState == HIGH) {
+    setLightState(LOW);
+  }
+}
+
 bool lightOnHandler(HomieRange range, String value) {
   if (value == "true") {
-    digitalWrite(PIN_RELAY, HIGH);
-    Homie.setNodeProperty(switchNode, "on").send("true");
-    Serial.println("Light is on");
-  //  switchState = true;
+    setLightState(HIGH);
   } else if (value == "false") {
-    digitalWrite(PIN_RELAY, LOW);
-    Homie.setNodeProperty(switchNode, "on").send("false");
-    Serial.println("Light is off");
-//    switchState = false;
+    setLightState(LOW);
   } else {
     Serial.print("Error Got: ");
     Serial.println(value);
@@ -36,22 +62,26 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println();
-  //pinMode(PIN_BUTTON,INPUT_PULLUP);
+  Serial.println("ecoplug");
+  pinMode(PIN_BUTTON,INPUT_PULLUP);
   pinMode(PIN_RELAY, OUTPUT);
   digitalWrite(PIN_RELAY, LOW);
   Homie.setLedPin(PIN_LED, LOW);
-  //Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
-  Homie_setFirmware("ecoplug", "1.0.0");
+  Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+  Homie_setBrand("ecoplug");
   switchNode.advertise("on").settable(lightOnHandler);
   button1.begin();
   Homie.setup();
 }
 
 void loop() {
+  uint8_t digitalReadTemp;
+  
   Homie.loop();
   if (button1.pressed())
   {
-    digitalWrite(PIN_RELAY, !digitalRead(PIN_RELAY));
+    invertLightState();
   }
   
 
